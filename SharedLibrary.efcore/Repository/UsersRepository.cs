70ea7698ca041ac7013066bfe1bf5c988200d41f
binna <every5116@naver.com>
2025-11-10 05:37:57 +0900

    [#14] 관심사 분리
     - Repository에 Context 의존 주입 에러 해결(커넥션 풀로 설정했는데 일반 Context로 해서 에러났었음)

--- SharedLibrary.efcore/Repository/UsersRepository.cs
+++ SharedLibrary.efcore/Repository/UsersRepository.cs
@@ -5,30 +5,37 @@ namespace SharedLibrary.Efcore.Repository;
 
 public class UsersRepository : IUsersRepository
 {
-    private readonly AppDbContext mAppDbContext;
+    private readonly IDbContextFactory<AppDbContext> mDbContextFactory;
 
-    public UsersRepository(AppDbContext appDbContext)
+    public UsersRepository(IDbContextFactory<AppDbContext> dbContextFactory)
     {
-        mAppDbContext = appDbContext;
+        mDbContextFactory = dbContextFactory;
     }
 
     public async Task<Users?> FindUserByEmailAsync(string email)
     {
-        return await mAppDbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+        // 매번 새 커넥션을 쓰는 것처럼 보이지만
+        // 드라이브에서 커넥션 풀까지 코드가 짜여 있기 때문에
+        // 커넥션 풀 재사용 한다
+        await using var dbContext = await mDbContextFactory.CreateDbContextAsync();
+        return await dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
     }
     
     public async Task<bool> ExistsUserAsync(string email)
     {
-        return await mAppDbContext.Users.AnyAsync(u => u.Email == email);
+        await using var dbContext = await mDbContextFactory.CreateDbContextAsync();
+        return await dbContext.Users.AnyAsync(u => u.Email == email);
     }
 
     public async Task AddAsync(string email, string nickname, string password)
     {
-        await mAppDbContext.Users.AddAsync(new Users(nickname, email, password));
+        await using var dbContext = await mDbContextFactory.CreateDbContextAsync();
+        await dbContext.Users.AddAsync(new Users(email, nickname, password));
     }
     
     public async Task<int> SaveAsync()
     {
-        return await mAppDbContext.SaveChangesAsync();
+        await using var dbContext = await mDbContextFactory.CreateDbContextAsync();
+        return await dbContext.SaveChangesAsync();
     }
 }
