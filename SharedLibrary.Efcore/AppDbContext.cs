1f9f725638e74918aea07b8d608533326ba8371a
binna <every5116@naver.com>
2025-11-11 05:32:54 +0900

    네이밍 규칙에 맞게 변경

---
+++ SharedLibrary.Efcore/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Models;
+using SharedLibrary.Utils;
+
+namespace SharedLibrary.Efcore;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Users> Users { get; set; } = null!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Users>()
+            .HasKey(u => u.UserId);
+    }
+
+     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+     {
+         configurationBuilder
+             .Properties<CustomDateTime>().HaveConversion<CustomDateTimeConverter>();
+     }
+}   
---
+++ SharedLibrary.Efcore/Converter/CustomDateTimeConverter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SharedLibrary.Utils;
+
+public class CustomDateTimeConverter : ValueConverter<CustomDateTime, DateTime>
+{
+    public CustomDateTimeConverter()
+        : base(time => time.DateTime, time => new CustomDateTime(time))
+    { }
+}
