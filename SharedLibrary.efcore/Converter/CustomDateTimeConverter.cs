12ba7b628d4aa781cf007512a95af5e09b68c8f8
binna <every5116@naver.com>
2025-11-11 07:09:00 +0000

    Delete SharedLibrary.efcore directory
    - 중복 저장 삭제

--- SharedLibrary.efcore/Converter/CustomDateTimeConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using SharedLibrary.Utils;
-
-public class CustomDateTimeConverter : ValueConverter<CustomDateTime, DateTime>
-{
-    public CustomDateTimeConverter()
-        : base(time => time.DateTime, time => new CustomDateTime(time))
-    { }
-}
