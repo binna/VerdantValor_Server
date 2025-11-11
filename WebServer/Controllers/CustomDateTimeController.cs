0bd9d5e198a9d2a038fd0551fe7f25861baf1413
binna <every5116@naver.com>
2025-11-11 15:49:09 +0900

    [#15] DateTime을 래핑한 CustomDateTime 구현
    - 시간을 바꾸고, 시간을 리셋을 위한 컨트롤러

---
+++ WebServer/Controllers/CustomDateTimeController.cs
@@ -0,0 +1,34 @@
+﻿#if DEVELOPMENT
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Common;
+using SharedLibrary.Protocol.Common;
+using SharedLibrary.Protocol.DTOs;
+using SharedLibrary.Utils;
+
+namespace WebServer.Controllers;
+
+[Route($"{AppConstant.WEB_SERVER_API_BASE}/[controller]")]
+[ApiController]
+public class CustomDateTimeController : Controller
+{
+    [HttpPost("set")]
+    public ApiResponse SetCustomNow([FromBody] SetCustomNowReq req)
+    {
+        CustomDateTime.SetCustomNow(
+            CustomDateTime.ToCustomDateTime(req.TargetNow)
+        );
+        
+        return new ApiResponse(ResponseStatus.FromResponseStatus(
+            EResponseStatus.Success, AppEnum.ELanguage.Ko));
+    }
+    
+    [HttpPost("reset")]
+    public ApiResponse ResetCustomNow()
+    {
+        CustomDateTime.ResetCustomNow();
+        
+        return new ApiResponse(ResponseStatus.FromResponseStatus(
+            EResponseStatus.Success, AppEnum.ELanguage.Ko));
+    }
+}
+#endif
