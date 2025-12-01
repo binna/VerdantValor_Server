0855de4e6a1eab6450322feabd6d7dc8ca733670
binna <every5116@naver.com>
2025-12-01 19:14:04 +0900

    [#23] 에코 서버
    - TCP Stream 연결
    - 클라이언트에서 Send 보내면
      서버에서 받고 Exit 인지 확인하고 아니면 다시 클라로 Send 보내는 기능 추가

---
+++ Echo.Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var clientSocket =
+    new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var endPoint = 
+    new IPEndPoint(IPAddress.Loopback, 20000);
+
+clientSocket.Connect(endPoint);
+Console.WriteLine("클라: 연결 성공");
+
+while (true)
+{
+    var str = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(str))
+        return;
+
+    if (str.Equals("exit", StringComparison.OrdinalIgnoreCase))
+        return;
+
+    var buffer = Encoding.UTF8.GetBytes(str);
+    clientSocket.Send(buffer);
+
+    var buffer2 = new byte[256];
+    var bytesRead = clientSocket.Receive(buffer2);
+    
+    if (bytesRead < 1)
+    {
+        Console.WriteLine("서버의 연결 종료");
+        return;
+    }
+    
+    var str2 = Encoding.UTF8.GetString(buffer2);
+    Console.WriteLine($"받음 : {str2}");
+}
---
+++ Echo.Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var serverSocket =
+    new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var endPoint = 
+    new IPEndPoint(IPAddress.Loopback, 20000);
+
+// 서버 소켓에 ip, port 할당
+serverSocket.Bind(endPoint);
+
+Console.WriteLine("서버: Listen 시작");
+
+// 클라이언트들의 연결 요청을 대기하는 상태로 만듦
+// 백로그큐 = 클라이언트들의 연결 요청 대기실
+// 20 : 백로그큐 Size로 보면 됨
+serverSocket.Listen(20);
+
+Console.WriteLine("서버: Accept 대기");
+
+// 백로그큐에서 하나 꺼내와서 연결 요청을 수락
+// 클라이언트와 데이터 통신을 위해 소켓 만듦
+using var clientSocket = serverSocket.Accept();
+Console.WriteLine("서버: 클라 접속됨");
+Console.WriteLine($"연결됨: {clientSocket.RemoteEndPoint}");
+
+while (true)
+{
+    var buffer = new byte[256];
+    var totalBytes = clientSocket.Receive(buffer);
+
+    if (totalBytes < 1)
+    {
+        Console.WriteLine("클라이언트의 종료");
+        return;
+    }
+
+    var str = Encoding.UTF8.GetString(buffer);
+    Console.WriteLine(str);
+    
+    clientSocket.Send(buffer);
+}
