81fdd0d62e857bc35d16f23be313dad0621a9770
binna <every5116@naver.com>
2026-02-14 17:09:03 +0900

    [#35] Swap Queue과 Distributed Lock 구현
    - 초안 스왑 큐 구현
    - 패킷 처리 흐름을 스왑 큐 기반으로 구성
    - 네트워크 수신을 가정한 Fake 패킷 생성 (스레드 기반, 타입/생성 간격 랜덤)

---
+++ Lock/SwapQueue/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace SwapQueue;
+
+enum EPacket
+{
+    Move,
+    Attack,
+    Heal,
+    Death,
+    UseItem,
+}
+
+class Packet
+{
+    public EPacket PacketType { get; set; }
+}
+
+class Program
+{
+    static Queue<Packet> WriteQueue;
+    static Queue<Packet> ReadQueue;
+    
+    static Random packetRandom = new Random();
+    static Random generatePacketRandom = new Random();
+    
+    static async Task Main(string[] args)
+    {
+        WriteQueue = new Queue<Packet>();
+        ReadQueue = new Queue<Packet>();
+
+        _ = GeneratePacketAsync();
+        _ = GeneratePacketAsync();
+
+        while (true)
+        {
+            Console.WriteLine($"읽기 큐에 {ReadQueue.Count}개 들어있음=====================");
+            
+            if (ReadQueue.Count > 0)
+                ProcessPacket();
+            else
+                await Task.Delay(1000);
+
+            Console.WriteLine("===========================================");
+            
+            (WriteQueue, ReadQueue) = (ReadQueue, WriteQueue);
+        }
+    }
+    
+    static async Task GeneratePacketAsync()
+    {
+        var packetCnt = Enum.GetValues<EPacket>().Length;
+        
+        while (true)
+        {
+            var packetType = (EPacket)packetRandom.Next(packetCnt);
+            WriteQueue.Enqueue(new Packet { PacketType = packetType });
+            
+            await Task.Delay(generatePacketRandom.Next(500));
+        }
+    }
+
+    static void ProcessPacket()
+    {
+        while (ReadQueue.Count > 0)
+        {
+            var packet = ReadQueue.Dequeue();
+            Console.WriteLine($"{packet.PacketType} 패킷 처리 완료");
+        }
+    }
+}
