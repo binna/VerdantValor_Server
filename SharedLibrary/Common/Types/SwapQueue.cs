f23249fa83e00358ff124f039a953e95a26d9c58
binna <every5116@naver.com>
2026-02-23 21:11:25 +0900

    [#35] Swap Queue과 Distributed Lock 구현
    - 분산 락을 라이브러리 형태로 구현

---
+++ SharedLibrary/Common/Types/SwapQueue.cs
@@ -0,0 +1,55 @@
+﻿namespace Common.Types;
+
+public class SwapQueue<T>
+{
+    private Queue<T> mInputQueue = new();
+    private Queue<T> mOutputQueue = new();
+    
+    private readonly object mLock = new();
+    
+    // 0: false
+    // 1: true
+    private int mProcessing;
+
+    // 생산자 (여러 스레드에서 패킷을 넣을 수 있음)
+    public void Enqueue(T packet)
+    {
+        lock (mLock)
+        {
+            mInputQueue.Enqueue(packet);
+        }
+    }
+
+    // 소비자 (보통 한 스레드)
+    // 한번에 모아서 처리
+    public void Process(Action<T> handlePacket)
+    {
+        if (handlePacket == null)
+            throw new ArgumentNullException(nameof(handlePacket));
+        
+        if (Interlocked.Exchange(ref mProcessing, 1) == 1)
+            return;
+
+        try
+        {
+            lock (mLock)
+            {
+                if (mInputQueue.Count == 0)
+                    return;
+
+                (mInputQueue, mOutputQueue) = (mOutputQueue, mInputQueue);
+            }
+
+            // 락 없이 outputQueue 전부 처리
+            while (mOutputQueue.Count > 0)
+            {
+                var packet = mOutputQueue.Dequeue();
+                handlePacket(packet);
+            }
+        }
+        finally
+        {
+            Interlocked.Exchange(ref mProcessing, 0);
+        }
+    }
+}
