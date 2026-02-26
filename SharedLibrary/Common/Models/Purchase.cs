df388f7ae229170a00238b8695968bf846424a2c
binna <every5116@naver.com>
2026-02-27 05:08:21 +0900

    [#37] Lock TTL 만료로 인한 동시 접근 문제
    - 구매 테이블 초안

---
+++ SharedLibrary/Common/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using Common.Types;
+using Shared.Types;
+
+namespace Common.Models;
+
+public class Purchase
+{
+    public int PurchaseId { get; private set; }
+    
+    public int StoreId { get; private set; }
+    
+    public EPurchaseState PurchaseState { get; private set; }
+    
+    public ulong UserId { get; private set; }   // FK
+    
+    public ServerDateTime CreatedAt { get; private set; }
+    
+    public ServerDateTime UpdatedAt { get; private set; }
+    
+    public ServerDateTime? CompletedAt { get; private set; }
+}
