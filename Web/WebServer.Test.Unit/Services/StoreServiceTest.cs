74e4c94ef3af038f977300c483fa864eb22c2d1a
binna <every5116@naver.com>
2026-03-27 08:46:10 +0900

    [#39] StoreServiceTest 초안
    - StoreServiceTest 클래스 생성

---
+++ Web/WebServer.Test.Unit/Services/StoreServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit.Abstractions;
+
+namespace WebServer.Test.Unit.Services;
+
+[Collection("GlobalSetup ResponseStatus")]
+public class StoreServiceTest
+{
+    private readonly ITestOutputHelper mOutput;
+
+    public StoreServiceTest(ITestOutputHelper output)
+    {
+        mOutput = output;
+    }
+}
