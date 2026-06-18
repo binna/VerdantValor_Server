c42e3db8ed32600b2c8fe264afa7eb838f2723f5
binna <every5116@naver.com>
2026-06-18 13:31:26 +0900

    [#43] Redis 기반 로그인 세션 관리 구현
    - Redis 세션 구조 추가

---
+++ SharedLibrary/Common/Types/UserSessionInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Types;
+
+public struct UserSessionInfo
+{
+    public string SessionId { get; set; }
+    public string DeviceId { get; set; }
+    public string ChatServerIp { get; set; }
+    public string BattleServerIp { get; set; }
+}
